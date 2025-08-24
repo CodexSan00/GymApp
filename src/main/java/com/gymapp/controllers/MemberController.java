506a4bca009b59d5ea58a70834b7cc22665ff64e
Santiago :v <santiagosancheznieva@gmail.com>
2025-08-24 12:33:21 -0300

    Add member class & Member Controller

--- src/main/java/com/gymapp/controllers/MemberController.java
+++ src/main/java/com/gymapp/controllers/MemberController.java
@@ -1,4 +1,46 @@
 package com.gymapp.controllers;
 
+import com.gymapp.models.Member;
+
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
 public class MemberController {
+    private List<Member> members = new ArrayList<>();
+    private int nextId = 0; //just for simulate auto-increment
+
+    public void register(String name, String lastName, String phone, String email, Date joinDate){
+        Member m = new Member(nextId++, name, lastName, phone, email, joinDate);
+        members.add(m);
+        System.out.println("\nMember " + m.getName() +" registered!");
+        m.showInfo();
+    }
+    public void listAllMembers(){
+        if(members.isEmpty()){
+            System.out.println("List is empty.");
+        } else {
+            for (Member m: members){
+                m.showInfo();
+            }
+        }
+    }
+    public void edit(int id, String name, String lastName, String phone, String email, Date joinDate){
+        for(Member m: members){
+            if(m.getId() == id){
+                m.setName(name);
+                m.setLastName(lastName);
+                m.setPhone(phone);
+                m.setEmail(email);
+                m.setJoinDate(joinDate);
+            }
+        }
+    }
+    public void delete(int id){
+        boolean toRemove = members.removeIf(members -> members.getId() == id);
+        if(toRemove){
+            System.out.println("Member removed.");
+        } else {
+            System.out.println("Member not found.");
+        }
+    }
 }
--- src/main/java/com/gymapp/models/Member.java
+++ src/main/java/com/gymapp/models/Member.java
@@ -1,4 +1,76 @@
 package com.gymapp.models;
 
+import java.util.Date;
+
 public class Member {
+    private int id;
+    private String name;
+    private String lastName;
+    private String phone;
+    private String email;
+    private Date joinDate;
+
+    public Member(){}
+
+    public Member(int id, String name, String lastName, String phone, String email, Date joinDate){
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+        this.joinDate = joinDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String tel) {
+        this.phone = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(Date joinDate) {
+        this.joinDate = joinDate;
+    }
+
+    public void showInfo(){
+        System.out.println("\nName: " + name + "\nLastname: " + lastName +
+                "\nPhone: " + phone + "\nEmail: " + email + "\nJoinDate: " + joinDate);
+    }
 }
