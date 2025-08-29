f727e7c92e604acfa5170bf46e7fe0d658b49b54
Santiago :v <santiagosancheznieva@gmail.com>
2025-08-29 19:10:25 -0300

    Refactor MemberController: CRUD methods return objects or booleans.

--- src/main/java/com/gymapp/controllers/MemberController.java
+++ src/main/java/com/gymapp/controllers/MemberController.java
@@ -9,22 +9,15 @@ public class MemberController {
     private List<Member> members = new ArrayList<>(); //This will be removed in the future
     private int nextId = -1; //just for simulate auto-increment
 
-    public void register(String name, String lastName, String phone, String email, Date joinDate){
+    public Member registerMember(String name, String lastName, String phone, String email, Date joinDate){
         Member m = new Member(nextId++, name, lastName, phone, email, joinDate);
         members.add(m);
-        System.out.println("\nMember " + m.getName() +" registered!");
-        m.toString();
+        return m;
     }
-    public void listAllMembers(){
-        if(members.isEmpty()){
-            System.out.println("List is empty.");
-        } else {
-            for (Member m: members){
-                m.toString();
-            }
-        }
+    public List<Member> listAllMembers(){
+        return new ArrayList<>(members);
     }
-    public void edit(int id, String name, String lastName, String phone, String email, Date joinDate){
+    public boolean editMember(int id, String name, String lastName, String phone, String email, Date joinDate){
         for(Member m: members){
             if(m.getId() == id){
                 m.setName(name);
@@ -32,15 +25,18 @@ public class MemberController {
                 m.setPhone(phone);
                 m.setEmail(email);
                 m.setJoinDate(joinDate);
+                return true;
             }
         }
+        return false;
     }
-    public void delete(int id){
-        boolean toRemove = members.removeIf(members -> members.getId() == id);
-        if(toRemove){
-            System.out.println("Member removed.");
-        } else {
-            System.out.println("Member not found.");
+    public boolean deleteMember(int memberId){
+        for(int i = 0; i < members.size(); i++){
+            if(members.get(i).getId() == memberId){
+                members.remove(i);
+                return true;
+            }
         }
+        return false;
     }
 }
