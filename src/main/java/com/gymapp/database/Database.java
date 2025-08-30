e885e823cf384f4b38516678cd1ccceef0d1954e
Santiago :v <santiagosancheznieva@gmail.com>
2025-08-30 16:51:03 -0300

    Add: Database connection class -Adds Database.java to manage connections with PostgreSQL -Uses environment variables for DB_URL, DB_USER and DB_PASS -Provides a getConnection() method returning a Connection object -Prepares the project for DAO integration

--- src/main/java/com/gymapp/database/Database.java
+++ src/main/java/com/gymapp/database/Database.java
@@ -1,4 +1,30 @@
 package com.gymapp.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Database {
+    private static Connection conn;
+
+    public static Connection getConnection() throws SQLException{
+        if(conn == null || conn.isClosed()){
+            //Read user & password from env variables
+            // Do "set DB_USER=YourUser" in CMD, same for URL or PASS (without:")
+            String url = System.getenv("DB_URL");
+            String user = System.getenv("DB_USER");
+            String password = System.getenv("DB_PASS");
+
+            if( url == null || user == null || password == null){
+                throw new RuntimeException("Please define env variable: DB_URL, DB_USER & DB_PASS");
+            }
+            conn = DriverManager.getConnection(url, user, password);
+        }
+        return conn;
+    }
+    public static void closeConnection() throws SQLException{
+        if(conn != null && !conn.isClosed()){
+            conn.close();
+        }
+    }
 }
