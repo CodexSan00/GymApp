86277d446799a8fc8c67e8830b15f4315d67fabf
Santiago :v <santiagosancheznieva@gmail.com>
2025-10-18 20:07:54 -0300

    Refactor: Database.java * Now loads configuration from .env using dotenv-java * Added try-catch block to handle database connection errors gracefully

--- src/main/java/com/gymapp/database/Database.java
+++ src/main/java/com/gymapp/database/Database.java
@@ -1,24 +1,29 @@
 package com.gymapp.database;
-
+import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Database {
     private static Connection conn;
-
+    private static final Dotenv dotenv = Dotenv.load();
     public static Connection getConnection() throws SQLException{
         if(conn == null || conn.isClosed()){
             //Read user & password from env variables
-            // Do "set DB_USER=YourUser" in CMD, same for URL or PASS (without:")
-            String url = System.getenv("DB_URL");
-            String user = System.getenv("DB_USER");
-            String password = System.getenv("DB_PASS");
+            String url = dotenv.get("DB_URL");
+            String user = dotenv.get("DB_USER");
+            String password = dotenv.get("DB_PASS");
 
             if( url == null || user == null || password == null){
-                throw new RuntimeException("Please define env variable: DB_URL, DB_USER & DB_PASS");
+                throw new RuntimeException("Missing DB_URL, DB_USER, or DB_PASS in .env");
             }
-            conn = DriverManager.getConnection(url, user, password);
+            try {
+                conn = DriverManager.getConnection(url, user, password);
+                System.out.println("Connected to database successfully...");
+            } catch (SQLException e) {
+                System.err.println("Database connection failed: " + e.getMessage());
+                throw e;
+            }
         }
         return conn;
     }
